345843a4b1e60a5b8f52fbab8b0f80dc1ac7cd2b
thxtomslf <75722306+thxtomslf@users.noreply.github.com>
2021-11-25 23:42:02 +0300

    v2.0

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def triangle(a, b, c):
+    sides = [a, b, c]
+    longest_side = max(sides)
+    sides.remove(longest_side)
     if a <= 0 or b <= 0 or c <= 0:
         return False, None
     if (a + b > c) and (a + c > b) and (b + a > a):
-        if a == b == c:
-            return True, "Равносторонний"
-        if a == b or b == c or c == a:
-            return True, "Равнобедренный"
-        return True, "Общего вида"
+        if longest_side ** 2 == sides[0] ** 2 + sides[1] ** 2:
+            return True, "Прямоугольный"
+        elif longest_side ** 2 > sides[0] ** 2 + sides[1] ** 2:
+            return True, "Тупоугольный"
+        else:
+            return True, "Остроугольный"
     else:
         return False, None
